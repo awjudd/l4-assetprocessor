6e20558ec18e210d71af8936a24ec61e9f894bbf
Andrew Judd <awjudd@gmail.com>
2013-12-31 00:29:24 -0500

    Adding in an error message if there is more than asset type for your instance

--- src/lang/en/errors.php
+++ src/lang/en/errors.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    'different-asset-types' => 'The specified file (:file) has more than one asset type associated with it.',
     'duplicate-name' => 'The specified name (:name) has been duplicated.',
     'file-not-found' => 'The specified file (:file) was not found.',
     'invalid-type' => 'Invalid asset class provided (:class) must implement ":interface".'
