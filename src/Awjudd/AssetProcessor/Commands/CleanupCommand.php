e04e0bd8b5e92b4fc8e916ed3b2d4283848c6379
Andrew Judd <awjudd@gmail.com>
2014-01-02 12:46:34 -0500

    Adding in the bulk of the logic for the cleanup command

--- src/Awjudd/AssetProcessor/Commands/CleanupCommand.php
+++ src/Awjudd/AssetProcessor/Commands/CleanupCommand.php
@@ -1,5 +1,6 @@
 <?php namespace Awjudd\AssetProcessor\Commands;
 
+use Awjudd\AssetProcessor\AssetProcessor;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -50,6 +51,67 @@ class CleanupCommand extends Command
             $duration = \Config::get('assetprocessor::cache.duration');
         }
 
-        // 
+        // Grab the minimum acceptable timestamp
+        $timestamp = time() - $duration;
+
+        // Get the full list of files
+        $files = $this->buildFileList(\AssetProcessor::storageFolder());
+
+        // Cycle through the list checking their creation dates
+        foreach($files as $file)
+        {
+            // Compare the file modification times
+            if(filemtime($file) < $timestamp)
+            {
+                // It passes the acceptable, so remove it
+                unlink($file);
+
+                // Check if the directory containing this file is empty
+                if(count(scandir(dirname($file))) == 2)
+                {
+                    rmdir(dirname($file));
+                }
+            }
+        }
+        
+    }
+
+    /**
+     * 
+     *
+     * @return array
+     */
+    private function buildFileList($folder)
+    {
+        // The list of files to use
+        $files = [];
+
+        // Cycle through all of the files in our storage folder removing
+        // any files that exceed the cache duration.
+        $directory = new \DirectoryIterator($folder);
+
+        foreach ($directory as $file)
+        {
+            // Check if the file is the local file (i.e. dot)
+            if($file->isDot())
+            {
+                // We are the dot, so just skip
+                continue;
+            }
+
+            // Check if the file is a directory
+            if($file->isDir())
+            {
+                // Recursively call this function
+                $files = array_merge($files, $this->buildFileList($file->getRealPath()));
+            }
+            else
+            {
+                // Add in the file
+                $files[] = $file->getRealPath();
+            }
+        }
+
+        return $files;
     }
 }
