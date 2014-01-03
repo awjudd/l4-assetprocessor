ff386e001243775d6f355904c217d18b8542ffb6
Andrew Judd <awjudd@gmail.com>
2014-01-03 00:01:56 -0500

    Fixing the indentation

--- src/Awjudd/AssetProcessor/AssetProcessorServiceProvider.php
+++ src/Awjudd/AssetProcessor/AssetProcessorServiceProvider.php
@@ -6,14 +6,14 @@ use Illuminate\Support\ServiceProvider;
 class AssetProcessorServiceProvider extends ServiceProvider
 {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
+    /**
      * Bootstrap the service provider.
      *
      * @return void
@@ -23,29 +23,29 @@ class AssetProcessorServiceProvider extends ServiceProvider
         $this->package('awjudd/assetprocessor');
     }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-	   // Register the application bindings
-       $this->registerBindings();
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register the application bindings
+        $this->registerBindings();
 
-       // Register the artisan commenads
-       $this->registerCommands();
-	}
+        // Register the artisan commenads
+        $this->registerCommands();
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('assetprocessor');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('assetprocessor');
+    }
 
     /**
      * Register the application bindings that are required.
